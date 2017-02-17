f2c7d7fae12f267422cc4061cb08387da8c9f327
snov <snov.contact@gmail.com>
2017-02-18 01:20:52 +0700

    added firebase module and config

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Created by snov on 18.02.2017.
+//
+// ABOUT THIS FILE
+//
+//=========================================================================
+
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyB_jp9rinp2qbjM8RadMRqyuTTBA4Jhim8",
+  authDomain: "sonsnov-e8a3a.firebaseapp.com",
+  databaseURL: "https://sonsnov-e8a3a.firebaseio.com",
+  storageBucket: "sonsnov-e8a3a.appspot.com",
+  messagingSenderId: "1083949478942"
+};
+
+const app = firebase.initializeApp(config);
