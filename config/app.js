3e40fd85658d592bd3eb2793b4a1aef45b283421
snov <snov.contact@gmail.com>
2017-02-19 02:21:47 +0700

    description updated

--- config/app.js
+++ config/app.js
@@ -6,15 +6,15 @@
 
 module.exports = {
   head: {
-    title: 'React Osnova Environment',
-    description: 'React-redux project boilerplate.',
+    title: 'React Redux Firebase App',
+    description: 'react redux firebase sample project.',
     meta: [
-      { name: 'description', content: 'React-redux project boilerplate.'},
+      { name: 'description', content: 'react redux firebase sample project.'},
       { charset: 'utf-8'},
-      { property: 'og:site_name', content: 'React Osnova Environment'},
+      { property: 'og:site_name', content: 'React Redux Firebase App'},
       { property: 'og:locale', content: 'en_US'},
-      { property: 'og:title', content: 'React Osnova Environment'},
-      { property: 'og:description', content: 'React-redux project boilerplate.'},
+      { property: 'og:title', content: 'React Redux Firebase App'},
+      { property: 'og:description', content: 'react redux firebase sample project.'},
       { property: 'og:card', content: 'summary'},
       { property: 'og:site', content: '@sonsnov'},
       { property: 'og:creator', content: '@sonsnov'}
